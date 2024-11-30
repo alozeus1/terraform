39c0cee4a9d5a6386a72d90276930ca96e26c370
alozeus1 <alozeus1@gmail.com>
2024-11-30 00:12:23 -0700

    s3 backend

--- awsec2.tf
+++ awsec2.tf
@@ -1,10 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket  = "alo-terraformstatefile"
+    key     = "build/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "del-admin"
+  }
+}
+
 resource "aws_instance" "terraform-demo" {
   ami           = "ami-0022f774911c1d690"
   instance_type = "t2.micro"
   key_name      = "demo"
+
   tags = {
-    Name = "Jenkins"
-    Env  = "dev"
+    Name  = "Jenkins"
+    Env   = "dev"
     Owner = "alo"
   }
 }
